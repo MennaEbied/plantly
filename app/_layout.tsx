42cc0c555a7bf14689c3468b6a95eb38cc063d63
MennaEbied <mennahesham253@gmail.com>
2024-10-18 18:05:09 +0300

    Set up bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,32 @@
-import {Stack} from "expo-router";
+import {Tabs} from "expo-router";
 import { View } from "react-native/types";
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import { theme } from "../theme";
 
 export default function Layout(){
     return(
-        <Stack>
-            <Stack.Screen 
+        <Tabs screenOptions={{tabBarActiveTintColor:theme.colorGreen}}>
+            <Tabs.Screen 
               name="index" 
               options={{
+                tabBarShowLabel:false,
                 title:"Home",
+                tabBarIcon: ({size,color}) =>(
+                    <Entypo name="leaf" size={size} color={color} />
+                )
               }}
             />
-        </Stack>
+             <Tabs.Screen 
+              name="profile" 
+              options={{
+                tabBarShowLabel:false,
+                title:"profile",
+                tabBarIcon:({size,color})=>(
+                    <Feather name="user" size={size} color={color} />
+                )
+              }}
+            />
+        </Tabs>
     )
 }
