0e0155eade2ee5e7a202117872849d27a65676d7
MennaEbied <mennahesham253@gmail.com>
2024-10-22 16:45:55 +0300

    Pick and store the plant image

--- app/new.tsx
+++ app/new.tsx
@@ -5,15 +5,19 @@ import {
     Alert,
     ScrollView,
     View,
+    TouchableOpacity,
+    Platform,
   } from "react-native";
-  import { theme } from "@/theme";
-  import { PlantlyButton } from "@/component/PlantlyButton";
-  import { useState } from "react";
-  import { PlantlyImage } from "@/component/PlantlyImage";
+import { theme } from "@/theme";
+import { PlantlyButton } from "@/component/PlantlyButton";
+import { useState } from "react";
+import { PlantlyImage } from "@/component/PlantlyImage";
 import { usePlantStore } from "@/store/plantsStore";
 import { useRouter } from "expo-router";
+import * as ImagePicker from "expo-image-picker";
   
   export default function NewScreen() {
+    const [imageUri,setImageUri]=useState<string>()
     const [name, setName] = useState<string>();
     const [days, setDays] = useState<string>();
     const addPlant=usePlantStore((state)=>state.addPlant);
@@ -38,10 +42,25 @@ import { useRouter } from "expo-router";
         );
       }
 
-      addPlant(name,Number(days));
+      addPlant(name,Number(days),imageUri);
       router.navigate("/");
 
     };
+    const handleChooseImage= async ()=>{
+      if(Platform.OS==="web"){
+        return;
+      }
+
+      const result= await ImagePicker.launchImageLibraryAsync({
+        mediaTypes:ImagePicker.MediaTypeOptions.Images,
+        allowsEditing:true,
+        aspect:[1,1],
+        quality:1,
+      });
+      if(!result.canceled){
+        setImageUri(result.assets[0].uri)
+      }
+    }
   
     return (
       <ScrollView
@@ -49,9 +68,13 @@ import { useRouter } from "expo-router";
         contentContainerStyle={styles.contentContainer}
         keyboardShouldPersistTaps="handled"
       >
-        <View style={styles.centered}>
-          <PlantlyImage />
-        </View>
+        <TouchableOpacity 
+          style={styles.centered} 
+          activeOpacity={0.8}
+          onPress={handleChooseImage}
+        >
+          <PlantlyImage imageUri={imageUri}/>
+        </TouchableOpacity>
         <Text style={styles.label}>Name</Text>
         <TextInput
           value={name}
@@ -97,6 +120,8 @@ import { useRouter } from "expo-router";
     },
     centered: {
       alignItems: "center",
+      marginBottom:24,
+
     },
   });
   
